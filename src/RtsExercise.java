8f6cec23096b365f195273f49709c9a966bb2730
tsyed1999 <64174511+tsyed1999@users.noreply.github.com>
2021-11-23 22:00:32 -0500

    Final solution to the problems.

--- src/RtsExercise.java
+++ src/RtsExercise.java
@@ -1,42 +1,12 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-
+/**
+ * This class contains the methods aboveBelow and stringRotation
+ */
 public class RtsExercise {
-    public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList();
-        numbers.add(1);
-        numbers.add(5);
-        numbers.add(2);
-        numbers.add(1);
-        numbers.add(10);
-        numbers.add(13);
-        numbers.add(9);
-        numbers.add(76);
-        numbers.add(10);
-        numbers.add(35);
-        numbers.add(1);
-        numbers.add(3);
-        numbers.add(5);
-        numbers.add(6);
-        numbers.add(6);
-        numbers.add(6);
-        numbers.add(6);
-        numbers.add(7);
-
-        // 1,1,2,5,10
-        Map output = aboveBelow(numbers, 6);
-        Map output2 = aboveBelow(new ArrayList<Integer>(), 6);
-        System.out.println("above:" + output.get("above"));
-        System.out.println("below:" + output.get("below"));
-        System.out.println(output2);
-
-        System.out.println(stringRotation("MyString",2));
-        System.out.println(stringRotation("Tahirshah",5));
-        System.out.println(stringRotation("",5));
-        System.out.println(stringRotation("There",5));
-
-    }
-
     /**
      * This method returns the number of integers above and below the number given.
      * @param listOfNumbers Unsorted list of numbers.
@@ -45,9 +15,11 @@ public class RtsExercise {
      */
     public static Map aboveBelow(List<Integer> listOfNumbers, int number) {
         Collections.sort(listOfNumbers);
+        //Checking if the list is null or empty
         if (listOfNumbers.size() == 0 || listOfNumbers.isEmpty()) {
             return null;
         }
+        //Creating the Map to return
         String above = "above";
         String below = "below";
         Map<String, Integer> outputMap = new HashMap<>(){{
@@ -55,6 +27,7 @@ public class RtsExercise {
             put(below,0);
         }};
         int ctr;
+        //Iterating through the list until the first element greater than the target number
         for (ctr = 0; ctr < listOfNumbers.size(); ctr++) {
             if (listOfNumbers.get(ctr) < number) {
                 outputMap.replace(below,outputMap.get(below).intValue(),outputMap.get(below).intValue() + 1);
@@ -64,6 +37,7 @@ public class RtsExercise {
                 break;
             }
         }
+        //Adding the rest of the elements to the "above" key in the map
         outputMap.replace(above,outputMap.get(above).intValue(),listOfNumbers.size() - ctr);
         return outputMap;
     }
@@ -71,18 +45,21 @@ public class RtsExercise {
     /**
      * This method rotates a string to the right by the given amount.
      * @param original Original string to be rotated.
-     * @param number Number or spots to rotate each character.
+     * @param rotationNumber Number of spots to rotate each character.
      * @return The rotated string.
      */
-    public static String stringRotation (String original, int number) {
+    public static String stringRotation(String original, int rotationNumber) {
         String outputString = "";
-        if (original.isEmpty()) {
+        //Checking if the original string is empty or if the rotation number is greater than the string length
+        if (original.isEmpty() || (rotationNumber > original.length())) {
             return null;
         }
-        for (int ctr = original.length() - number; ctr < original.length(); ctr++){
+        //Adding the last 'rotationNumber' characters of the string to the output string.
+        for (int ctr = original.length() - rotationNumber; ctr < original.length(); ctr++){
             outputString += original.charAt(ctr);
         }
-        for (int ctr2 = 0; ctr2 < original.length() - number; ctr2++){
+        //Adding the remaining characters from the beginning to the output string.
+        for (int ctr2 = 0; ctr2 < original.length() - rotationNumber; ctr2++){
             outputString += original.charAt(ctr2);
         }
         return outputString;
